5c86dbfbea9e7ba5f6bdc571605748c60ac5fea5
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-08-04 12:10:19 +0900

    taskに関するroute設定、ミドルウェア追加

--- router/router.go
+++ router/router.go
@@ -1,8 +1,11 @@
 package router
 
 import (
+	"os"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
-	"github.com/trailstem/go-nextjs-rest-api/controller"
+	"github.com/trailstem/go-nextjs-rest-api/di"
 )
 
 type Router struct {
@@ -15,11 +18,26 @@ func NewRouter() *Router {
 	}
 }
 
-func (r *Router) SetupRoutesAndStart(uc controller.IUserController) {
+func (r *Router) SetupRoutesAndStart(ac *di.AppControllers) {
 
-	r.e.POST("/signup", uc.SignUp)
-	r.e.POST("/login", uc.LogIn)
-	r.e.POST("/logout", uc.LogOut)
+	r.e.POST("/signup", ac.UserController.SignUp)
+	r.e.POST("/login", ac.UserController.LogIn)
+	r.e.POST("/logout", ac.UserController.LogOut)
+
+	// ミドルウェア適用
+	// SigningKey:JWT作成時と同じkey指定
+	// TokenLookup: clientから送られてくるtoken格納場所を指定
+	t := r.e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", ac.TaskController.GetAllTasks)
+	t.GET("/:taskId", ac.TaskController.GetTaskById)
+	t.POST("", ac.TaskController.CreateTask)
+	t.PUT("/:taskId", ac.TaskController.UpdateTask)
+	t.DELETE("/:taskId", ac.TaskController.DeleteTask)
+
 	// サーバー起動
 	r.e.Logger.Fatal(r.e.Start(":8080"))
 }
